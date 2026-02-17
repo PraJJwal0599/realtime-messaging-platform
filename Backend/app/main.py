b69738db994e558c4313b84e9943722b3bc32c4d
Prajjwal Yadav <prajjwal@mac.speedport.ip>
2026-02-17 02:07:55 +0100

    CORS update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -23,10 +23,14 @@ app = FastAPI(title = APP_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5500"],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins=[
+        "http://localhost:8000",
+        "http://127.0.0.1:5500",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class ConnectionManager:
